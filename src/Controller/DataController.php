4c7d803dce105e51a0c73452a25ee99c75d69ce6
Fix7g <fbasta7@gmail.com>
2018-09-28 14:07:39 +0200

    update

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 use App\Entity\Users;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\AbstractType;
@@ -21,70 +20,61 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class DataController extends AbstractController
 {
-
-    
-        /**
-        * @Route("/add")
-        * 
-        */
-        public function index(Request $request)
-        {   
-           
-            $user = new users();    
-            $form = $this->createFormBuilder($user)
-                ->add('Name', TextType::class)
-                ->add('Sname', TextType::class)
-                ->add('file', FileType::class)
-                ->add('save', ButtonType::class, array('label' => 'Wyslij'))
-                ->getForm();
-          
-                if ($request->isXmlHttpRequest()) {  
-                    
-                    $file = $request->files->get('file');
-                    $name = $request->get('name');
-                    $fname = $request->get('fname');
-                    $sname = $request->get('sname');
-                    $Ext = $file->guessExtension();
-                    if($Ext != 'png' and $Ext != 'jpg' and $Ext != 'jpeg'){
-                        return new JsonResponse("Zły format!");
-                    }
-                    else{
-                    $user ->setName($name);
-                    $user ->setSname($sname);
-                    $user ->setFile($fname);
-                    
-                    $request->files->get('file')->move(
-                        $this->getParameter('brochures_directory'),
-                        $fname
-                    );
-                   
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($user);
-                    $entityManager->flush();
-                    return new JsonResponse("Dane został dodane.");
+    /**
+    * @Route("/add")
+    * 
+    */
+    public function index(Request $request)
+    {   
+        $user = new users();    
+        $form = $this->createFormBuilder($user)
+            ->add('Name', TextType::class)
+            ->add('Sname', TextType::class)
+            ->add('file', FileType::class)
+            ->add('save', ButtonType::class, array('label' => 'Wyslij'))
+            ->getForm();
+        
+            if ($request->isXmlHttpRequest()) 
+            {                    
+                $file = $request->files->get('file');
+                $name = $request->get('name');
+                $fname = $request->get('fname');
+                $sname = $request->get('sname');
+                $Ext = $file->guessExtension();
+                if($Ext != 'png' and $Ext != 'jpg' and $Ext != 'jpeg'){
+                    return new JsonResponse("Zły format!");
                 }
-            }
-            else{  
-            return $this->render('data/index.html.twig', array(
-                'form' => $form->createView()
-            ));
+                else{
+                $user ->setName($name);
+                $user ->setSname($sname);
+                $user ->setFile($fname);
+                
+                $request->files->get('file')->move(
+                    $this->getParameter('brochures_directory'),
+                    $fname
+                );
+                
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                return new JsonResponse("Dane został dodane.");
             }
         }
-
-
-
-
-
-
+        else{  
+        return $this->render('data/index.html.twig', array(
+            'form' => $form->createView()
+        ));
+        }
+    }
 
     /**
     * @Route("/log")
     * 
     */    
-    public function Login(Request $request) {  
+    public function Login(Request $request) 
+    {  
         $form = $this->createFormBuilder()
             ->add('Name', TextType::class)
             ->add('pass', PasswordType::class)
@@ -95,7 +85,8 @@ class DataController extends AbstractController
             $login = $request->get('login');
             $password = $request->get('password');
 
-            if($login == 'a' and $password == 'b'){
+            if($login == 'admin' and $password == 'admin')
+            {
                 $users = $this->getDoctrine()
                     ->getRepository(Users::class)
                     ->findAll();
@@ -103,7 +94,7 @@ class DataController extends AbstractController
             }
             else 
             {
-                return new Response("zle hasło");
+                return new Response("Zły login lub hasło!");
             }
         } 
         else 
@@ -123,8 +114,6 @@ class DataController extends AbstractController
         $userFile = $file->findOneBy(['id' => $id]);
         $userFileName  = $userFile->getFile();
         $publicResourcesFolderPath = $this-> getParameter('brochures_directory')."/" ;
- 
-
         return  new BinaryFileResponse($publicResourcesFolderPath.$userFileName);
     }
 }
